233ef140b8fad026917b2c68b4180c81a47a9ea5
Gniewo1 <136825015+Gniewo1@users.noreply.github.com>
2025-08-18 14:59:00 +0200

    Import template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Strona główna np. Dashboard lub Home */}
+        <Route path="/" element={<div>Home Page</div>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("http://127.0.0.1:8000/auth/login/", {
+      username,
+      password,
+    });
+    localStorage.setItem("token", res.data.token);
+    alert("Zalogowano jako " + res.data.user.username);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Login" />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Hasło" />
+      <button type="submit">Zaloguj</button>
+    </form>
+  );
+}
