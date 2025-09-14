3d2782808c1d673550764441ab1da907e4584819
Gniewo1 <136825015+Gniewo1@users.noreply.github.com>
2025-09-14 18:00:20 +0200

    item eaten model added

--- backend/items/models.py
+++ backend/items/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from users.models import CustomUser
 
 
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    cal_in_grams = models.DecimalField(max_digits=6, decimal_places=2)  ### calories in 100g
+    cal_in_gram = models.DecimalField(max_digits=6, decimal_places=4)  
     cal_in_portion = models.DecimalField(max_digits=6, decimal_places=2, default=None)
 
     def __str__(self):
         return self.name
     
 
+class EatenItem(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    portion = models.DecimalField(max_digits=6, decimal_places=2, default=None)
+    grams = models.DecimalField(max_digits=6, decimal_places=2, default=None)
+    date = models.DateField()
+
+    
